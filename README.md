fbee758b160e65ea5247665aab15a482eb03bbc1
bolderstephanie <bolderstephanie@gmail.com>
2020-11-10 12:20:47 +0100

    dashboard af, flat design

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Create a repository from the template. Clone your own repository.
 run `npm install` and then `npm start` for your dev server. 
 You can run `npm run setup` for some additional options and name of your website.
 
+# Weegschaal - Arduino
+Dit project is gemaakt om de inventarisatie van een bedrijf bij te houden. Hierbij is gebruik gemaakt van de Arduino UNO, en de 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Josefin sans', 'sans-serif'],
+        body: ['Graphik', 'sans-serif'],
+      } ,
+      colors: {
+        verydarkmagenda: '#502050' ,
+        darkgrayishmagenda: '#937B92' ,
+        lightgrayishmagenda: '#F4F6FA',
+        softpink: '#EE68A4',
+        pink: '#9C5A4E' ,
+        white: '#FFFFFF',
+        donkerzwart: '#4a4a4a' ,
+        button: '#CFAFAF' ,
+        lichtbeige: '#FFFAE2' ,
+        donkergrijs: '#080808' ,
+        lichtgrijs: '#9AA5A7' ,
+        achtergrondbeige: '#F6F5F1' ,
+        achtergrondgrijs: '#9AA5AB' ,
+        footergrijs: '#9AA5A7' ,
+      }
+    },
   },
   variants: {},
   plugins: [],
